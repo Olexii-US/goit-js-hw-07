2c959ac2f78335259be6506bc1bb4146317ef93e
Olexii-US <alyosha.us@gmail.com>
2022-10-16 23:01:30 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,11 +5,6 @@ console.log(galleryItems);
 
 const galleryBox = document.querySelector('.gallery')
 
-galleryBox.addEventListener('click', (event) => {
-    event.preventDefault()
-}    
-)
-
 const markupGallery = galleryItems.map((item) => `<div class="gallery__item">
     <a class="gallery__link" href="${item.original}">
         <img
@@ -31,23 +26,22 @@ linkRef.forEach(function (item) {
     event.preventDefault();})
 })
 
+galleryBox.addEventListener('click', (event) => {
+    const instance = basicLightbox.create(`
+        <img src=${event.target.dataset.source}>
+    `)
+    instance.show()
 
+    window.addEventListener('keydown', escapeModalClose);
 
-
-
-// galleryBox.innerHTML = ...htmlString
-
-// /////
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-//     description: 'Hokkaido Flower',
-//   },
-//   /////
-
-
+    function escapeModalClose(keyEvent) {
+        if (keyEvent.code === 'Escape') {
+        instance.close()
+            console.log(keyEvent)
+            window.removeEventListener('keydown', escapeModalClose)
+        }
+    }
+})
 
 
 
